a94f62205496f9c19367f67befb26e172c123181
tasueom <tasueom@naver.com>
2025-10-02 15:30:42 +0900

    feat: 비밀번호 해싱과 세션 사용 준비

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template as ren, request, redirect, url_for, session
+from werkzeug.security import generate_password_hash as gen_pw, check_password_hash as chk_pw
 
 app = Flask(__name__)
+app.secret_key = "secret_key_b"
 
 #Flask 서버 실행
 if __name__ == "__main__":
