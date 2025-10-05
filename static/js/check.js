0f0ec7c109cea8cced82e4ae567760605c5827ba
tasueom <tasueom@naver.com>
2025-10-05 21:56:29 +0900

    feat: 회원가입 입력 유효성 검사 추가

---
+++ static/js/check.js
@@ -0,0 +1,37 @@
+function checkUser(f) {
+    const uid = f.uid.value;
+    const pw = f.password.value;
+    const pwCheck = f.passwordCheck.value;
+    const phone = f.phone.value;
+    const email = f.email.value;
+
+    const uidPattern = /^[A-Za-z0-9]+$/;
+    const phonePattern = /^\d{3}-\d{4}-\d{4}$/;
+    const emailPattern = /^[\w.-]+@[\w.-]+\.[A-Za-z]{2,}$/;
+
+    if (!uidPattern.test(uid)) {
+        alert("아이디는 영어와 숫자만 입력 가능합니다.");
+        f.uid.focus();
+        return false;
+    }
+
+    if (pw !== pwCheck) {
+        alert("비밀번호가 일치하지 않습니다.");
+        f.password.focus();
+        return false;
+    }
+
+    if (!phonePattern.test(phone)) {
+        alert("전화번호 형식이 올바르지 않습니다. (예: 010-1234-5678)");
+        f.phone.focus();
+        return false;
+    }
+
+    if (!emailPattern.test(email)) {
+        alert("이메일 형식이 올바르지 않습니다.");
+        f.email.focus();
+        return false;
+    }
+
+    return true;
+}
